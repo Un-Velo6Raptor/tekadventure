d1f7075623aa9c06edffee0e453e01cd0b4150a7
Hugo Cousin <hugo.cousin@epitech.eu>
2017-05-22 14:37:01 +0200

    Refresh dir

---
+++ refresh/refresh.h
@@ -0,0 +1,14 @@
+/*
+** refresh.h for tekadventure in /home/heychsea/MUL/tekadventure/refresh
+** 
+** Made by Hugo Cousin
+** Login   <hugo.cousin@epitech.eu>
+** 
+** Started on  Mon May 22 14:34:35 2017 Hugo Cousin
+** Last update Mon May 22 14:36:18 2017 Hugo Cousin
+*/
+
+#ifndef REFRESH_H_
+# define REFRESH_H_
+
+#endif /* ! REFRESH_H_ */
