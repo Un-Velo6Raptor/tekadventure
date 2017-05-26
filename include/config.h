c05ace84af7d3e76c9ab1982462debd1feb66c88
Hugo Cousin <hugo.cousin@epitech.eu>
2017-05-26 13:50:42 +0200

    Enum for dirs

--- include/config.h
+++ include/config.h
@@ -5,7 +5,7 @@
 ** Login   <hugo.cousin@epitech.eu>
 ** 
 ** Started on  Fri May 26 12:03:54 2017 Hugo Cousin
-** Last update Fri May 26 12:06:16 2017 Hugo Cousin
+** Last update Fri May 26 13:50:09 2017 Hugo Cousin
 */
 
 #ifndef CONFIG_H_
@@ -20,5 +20,15 @@ typedef struct	s_config
   char		*name;
   int		(*func)(char *, size_t, size_t, char **);
 }		t_config;
+
+typedef enum	dir
+  {
+    MAP,
+    MAP_BACK,
+    BOSS,
+    PLAYER_SPRITE,
+    PLAYER_MENU,
+    PLAYER_TEXT
+  }		t_dir;
 
 #endif /* !CONFIG_H_ */
