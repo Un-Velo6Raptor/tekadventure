57f362323c55f79c2068cc78e42a99cbbd716bf8
Hugo Cousin <hugo.cousin@epitech.eu>
2017-05-28 20:34:13 +0200

    Tabs on bar

--- src/bar/disp.c
+++ src/bar/disp.c
@@ -5,7 +5,7 @@
 ** Login   <sahel.lucas-saoudi@epitech.eu>
 **
 ** Started on  Sun May 28 20:13:48 2017 Sahel Lucas--Saoudi
-** Last update Sun May 28 20:14:52 2017 Sahel Lucas--Saoudi
+** Last update Sun May 28 20:33:54 2017 Hugo Cousin
 */
 
 #include        <stdlib.h>
@@ -14,7 +14,7 @@
 #include        "game.h"
 #include        "refresh.h"
 
-void             disp_player_boss(t_needs *needs)
+void		disp_player_boss(t_needs *needs)
 {
   sfSprite_setPosition(needs->player[needs->current_player]->sprite,
                        vector_2f(100, 770));
@@ -26,15 +26,15 @@ void             disp_player_boss(t_needs *needs)
                     ->sprite, vector_2f(13.5, 13.5));
 }
 
-void             display_mike(t_needs *needs)
+void		display_mike(t_needs *needs)
 {
   sfRenderWindow_drawSprite(needs->window, needs->boss[0]->select, NULL);
 }
 
-void    disp_text_and_bar(t_needs *needs,
-                int *move,
-                int *c_moove,
-                sfSprite *cursor)
+void		disp_text_and_bar(t_needs *needs,
+				  int *move,
+				  int *c_moove,
+				  sfSprite *cursor)
 {
   sfRenderWindow_clear(needs->window, sfWhite);
   refresh_room(needs, 1);
@@ -43,10 +43,10 @@ void    disp_text_and_bar(t_needs *needs,
   update_dir_bar(c_moove, move, cursor);
 }
 
-void    choose_print(t_needs *needs,
-                     sfTexture *tr,
-                     t_framebuffer *pattern,
-                     int opt)
+void		choose_print(t_needs *needs,
+			     sfTexture *tr,
+			     t_framebuffer *pattern,
+			     int opt)
 {
   sfTexture_updateFromPixels(tr, pattern->pixels, WIDTH, HEIGHT, 0, 0);
   if (opt == 0)
@@ -55,7 +55,7 @@ void    choose_print(t_needs *needs,
     display_mike(needs);
 }
 
-void    disp_boss_text(t_needs *needs)
+void		disp_boss_text(t_needs *needs)
 {
   disp_player_boss(needs);
   print_text(needs);
